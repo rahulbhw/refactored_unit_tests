f98750bf8431ad5dccfd30e23d06d34d54fec1bd
Aritra Biswas <pandalearnstocode@gmail.com>
2023-03-03 09:41:09 +0530

    adding

--- data_module/configs/config.py
+++ data_module/configs/config.py
@@ -1,7 +1,8 @@
 def get_config():
     import os
     import yaml
-    config_path = os.path.join(os.getcwd(), 'configs', 'config.yaml')
-    with open(config_path, 'r') as f:
+
+    config_path = os.path.join(os.getcwd(), "configs", "config.yaml")
+    with open(config_path, "r") as f:
         config = yaml.safe_load(f)
     return config
